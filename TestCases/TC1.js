2a83e7eb272630ff143a1311501fac184c7bd3fb
Siddharth <siddharth.singh0701@gmail.com>
2021-11-03 16:57:21 +0530

    protractor testcases

---
+++ TestCases/TC1.js
@@ -0,0 +1,16 @@
+const { browser, element } = require("protractor")
+const { By } = require("selenium-webdriver")
+
+describe('My First Test Suite',function(){
+    it('My First Test Case',function(){
+
+      browser.get("https://my.dnb.com/login")
+      //browser.wait(3000)
+      
+      element(By.id('okta-signin-username')).sendKeys('prodtest.ankitkabrawalaabc.defgh@gmail.com')
+      element(By.id('okta-signin-password')).sendKeys('$Testing5670')
+      element(By.id('okta-signin-submit')).click()
+      browser.sleep(2000)
+    })
+    
+})
